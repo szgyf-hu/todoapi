7cc3a1862321e40393a560a2b8b2f7940b5dd626
Attila Kalcsú <attila.kalcsu@gmail.com>
2018-02-01 09:28:17 +0100

    sqlfiin

--- init.sql
+++ init.sql
@@ -3,30 +3,25 @@ USE todoapi;
 CALL addUser('Teszt Elek', 't.elek@matav.hu', SHA2('telek', 256));
 CALL addUser('Kerékgyártó Emil', 'kamil@freemail.hu', SHA2('kamilka123', 256));
 CALL addUser('Dékány Szabi', 'lol@lol.hu', SHA2('semmmi',256));
+SELECT * FROM user;
 
 INSERT INTO usergroup (name)
 VALUES
 ('egy'),
 ('ketto'),
 ('csipkebokorvesszo');
-
-Select * From todoapi.usergroup;
-
+SELECT * FROM todoapi.usergroup;
 
 INSERT INTO user_usergroup (user_id, usergroup_id)
 VALUES
-(0, 0),
-(0, 1),
-(2, 1),
+(1, 1),
 (1, 2),
-(2, 2);
+(3, 2),
+(2, 3),
+(3, 3);
 
-CALL addUserTodo(0, "Ültesd el a muskátlit", 1, CURRENT_TIMESTAMP, @todo_id);
-CALL addUserTodo(1, "Vegyél kenyeret", 0, CURRENT_TIMESTAMP, @todo_id);
-CALL addUserTodo(2, "Vegyél levegőt", 2, CURRENT_TIMESTAMP, @todo_id);
+CALL addUserTodo(1, "Ültesd el a muskátlit", 1, CURRENT_TIMESTAMP, @todo_id);
+CALL addUserTodo(2, "Vegyél kenyeret", 0, CURRENT_TIMESTAMP, @todo_id);
+CALL addUserTodo(3, "Vegyél levegőt", 2, CURRENT_TIMESTAMP, @todo_id);
 
 CALL addUserGroupTodo(2, "Valaki cseréljen égőt", 7, CURRENT_TIMESTAMP, @todo_id);
-
-INSERT INTO todo_usergroup( todo_id, usergroup_id)
-VALUES
-(3, 2);
